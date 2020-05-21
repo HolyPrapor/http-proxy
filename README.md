e3fcc47a55df9fb6231feacd9e36acfc7963be50
Zeliboba <tolya12345w@gmail.com>
2020-05-21 16:46:44 +0500

    Finished HTTP-proxy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# http-proxy
+# HTTP-proxy
+
+Simple python HTTP proxy with some kind of AD blocking features. (See filters.)
--- proxy.py
+++ proxy.py
@@ -1,150 +1,143 @@
 import socket
-import select
-import time
+import threading
+import signal
 import sys
+import re
+from http import HTTPStatus
+import argparse
 
-# Changing the buffer_size and delay, you can improve the speed and bandwidth.
+HOST_NAME = "127.0.0.1"
+BIND_PORT = 12345
+MAX_REQUEST_LEN = 1024
+CONNECTION_TIMEOUT = 5
 
-# But when buffer get to high or delay go too down, you can broke things
-
-buffer_size = 4096
-
-delay = 0.0001
-
-forward_to = ('smtp.zaz.ufsk.br', 25)
+VERBOSE = False
+filters = "filters.txt"
 
 
-class Forward:
+def handle_browser_request(connection_socket):
+    request = connection_socket.recv(MAX_REQUEST_LEN).decode('utf8')
 
+    url, webserver, port = extract_config_from_request(request)
+
+    with open(filters, 'r') as f:
+        for line in filter(lambda x: len(x), f.readlines()):
+            if re.match(rf'{line}', url):
+                connection_socket.sendall(HTTPStatus.FORBIDDEN)
+                return
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(CONNECTION_TIMEOUT)
+    s.connect((webserver, port))
+    s.sendall(request.encode('utf8'))
+
+    data = b''
+    buffer = b'stub'
+    while len(buffer):
+        buffer = s.recv(MAX_REQUEST_LEN)
+        data += buffer
+    connection_socket.sendall(data)
+
+
+def extract_config_from_request(request):
+    first_line = request.split('\n')[0]
+    splitted_line = first_line.split(' ')
+
+    request_type = splitted_line[0]
+
+    url = splitted_line[1]
+
+    if VERBOSE:
+        print(request_type, url)
+
+    http_pos = url.find("://")
+    if http_pos == -1:
+        rest_of_url = url
+    else:
+        rest_of_url = url[(http_pos + 3):]
+
+    port_pos = rest_of_url.find(":")
+
+    webserver_pos = rest_of_url.find("/")
+    if webserver_pos == -1:
+        webserver_pos = len(rest_of_url)
+
+    if port_pos == -1 or webserver_pos < port_pos:
+        port = 80
+        webserver = rest_of_url[:webserver_pos]
+    else:
+        port = int(
+            (rest_of_url[(port_pos + 1):])[:webserver_pos - port_pos - 1])
+        webserver = rest_of_url[:port_pos]
+    return url, webserver, port
+
+
+class Server:
     def __init__(self):
+        signal.signal(signal.SIGINT, self.shutdown)
+        self.serverSocket = socket.socket(socket.AF_INET,
+                                          socket.SOCK_STREAM)
+        self.serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.serverSocket.bind((HOST_NAME, BIND_PORT))
+        self.serverSocket.listen(10)
+        self.clients = []
 
-        self.forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def listen_for_client(self):
+        while True:
+            (clientSocket,
+             client_address) = self.serverSocket.accept()
 
-    def start(self, host, port):
+            self.clients.append(clientSocket)
 
-        try:
-            self.forward.connect((host, port))
-            return self.forward
-        except:
-            print("Exception occured!")
-            return False
+            threading.Thread(target=handle_browser_request,
+                             args=(clientSocket,), daemon=True).start()
 
-
-class TheServer:
-    input_list = []
-
-    channel = {}
-
-    def __init__(self, host, port):
-
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-        self.server.bind((host, port))
-
-        self.server.listen(200)
-
-    def main_loop(self):
-
-        self.input_list.append(self.server)
-
-        while 1:
-
-            time.sleep(delay)
-
-            ss = select.select
-
-            input_ready, output_ready, except_ready = ss(self.input_list, [], [])
-
-            for self.s in input_ready:
-
-                if self.s == self.server:
-                    self.on_accept()
-
-                    break
-
-                self.data = self.s.recv(buffer_size)
-
-                if len(self.data) == 0:
-
-                    self.on_close()
-
-                    break
-
-                else:
-
-                    self.on_recv()
-
-    def on_accept(self):
-
-        forward = Forward().start(forward_to[0], forward_to[1])
-
-        clientsock, clientaddr = self.server.accept()
-
-        if forward:
-
-            print(clientaddr, "has connected")
-
-            self.input_list.append(clientsock)
-
-            self.input_list.append(forward)
-
-            self.channel[clientsock] = forward
-
-            self.channel[forward] = clientsock
-
-        else:
-
-            print("Can't establish connection with remote server.")
-
-            print("Closing connection with client side", clientaddr)
-
-            clientsock.close()
-
-    def on_close(self):
-
-        print(self.s.getpeername(), "has disconnected")
-
-        # remove objects from input_list
-
-        self.input_list.remove(self.s)
-
-        self.input_list.remove(self.channel[self.s])
-
-        out = self.channel[self.s]
-
-        # close the connection with client
-
-        self.channel[out].close()  # equivalent to do self.s.close()
-
-        # close the connection with remote server
-
-        self.channel[self.s].close()
-
-        # delete both objects from channel dict
-
-        del self.channel[out]
-
-        del self.channel[self.s]
-
-    def on_recv(self):
-
-        data = self.data
-
-        # here we can parse and/or modify the data before send forward
-
-        print(data)
-
-        self.channel[self.s].send(data)
+    def shutdown(self):
+        for sock in self.clients:
+            sock.close()
+        self.serverSocket.close()
+        sys.exit(0)
 
 
-if __name__ == '__main__':
+def parse_args(args):
+    parser = argparse.ArgumentParser(description="Simple HTTP proxy\n")
+    parser.add_argument('-p', '--port', action='store', type=int,
+                        help='Proxy port. Default - 12345')
+    parser.add_argument('-f', '--filters', action='store', type=str,
+                        help='File with '
+                             'adblocker '
+                             'filters in '
+                             'regex format '
+                             'Default = '
+                             'filters.txt')
+    parser.add_argument('-v', '--verbose', action='store_true')
 
-    server = TheServer('', 9090)
+    parsed = parser.parse_args(args)
 
-    try:
-        server.main_loop()
-    except KeyboardInterrupt:
-        print("Ctrl C - Stopping server")
-        sys.exit(1)
+    if parsed.port and (parsed.port < 0 or parsed.port > 65535):
+        print("Please provide correct port.")
+        exit(1)
+
+    return parsed
+
+
+def main(args):
+    args = parse_args(args[1:])
+    if args.verbose:
+        global VERBOSE
+        VERBOSE = True
+
+    if args.port:
+        global BIND_PORT
+        BIND_PORT = args.port
+
+    if args.filters:
+        global filters
+        filters = args.filters
+    print("HTTP Proxy is starting.")
+    server = Server()
+    server.listen_for_client()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
