f5aea8f07f64e674770e82a3582d7ce24f22ed90
Zeliboba <tolya12345w@gmail.com>
2020-05-28 17:26:23 +0500

    Refactored and fixed proxying bug

--- proxy.py
+++ proxy.py
@@ -4,51 +4,118 @@ import signal
 import socket
 import sys
 import threading
+import select
 
 HOST_NAME = "127.0.0.1"
 BIND_PORT = 12345
-MAX_REQUEST_LEN = 1024
+MAX_BUFFER_SIZE = 65536
 CONNECTION_TIMEOUT = 5
+
+CONNECTION_ESTABLISHED_MESSAGE = b"HTTP/1.1 200 OK\r\n\r\n"
+FORBIDDEN_MESSAGE = b"HTTP/1.1 403 Forbidden\r\n\r\n"
+BAD_REQUEST_MESSAGE = b"HTTP/1.1 400 Bad Request\r\n\r\n"
+INTERNAL_SERVER_ERROR = b"HTTP/1.1 501 Internal Server Error\r\n\r\n"
 
 VERBOSE = False
 filters = "filters.txt"
 
 
 def handle_browser_request(connection_socket):
-    request = connection_socket.recv(MAX_REQUEST_LEN).decode('utf8')
+    connection_socket.settimeout(CONNECTION_TIMEOUT)
+    request = read_all(connection_socket)
+    # print("=> " + request.decode('utf8'))
 
     if not len(request):
         return
 
-    url, webserver, port = extract_config_from_request(request)
+    url, webserver, port, request_type = extract_config_from_request(request)
 
-    with open(filters, 'r') as f:
-        for line in filter(lambda x: len(x), f.readlines()):
-            if re.findall(rf'{line.rstrip()}', url):
-                connection_socket.sendall("HTTP/1.1 403 "
-                                          "Forbidden\r\nConnection: "
-                                          "Closed\r\n".encode('utf8'))
-                return
+    if is_url_filtered(url):
+        connection_socket.sendall(FORBIDDEN_MESSAGE)
+        connection_socket.close()
+        return
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(CONNECTION_TIMEOUT)
     try:
         s.connect((webserver, port))
-        s.sendall(request.encode('utf8'))
     except socket.error:
-        connection_socket.sendall(b'')
+        connection_socket.sendall(BAD_REQUEST_MESSAGE)
+        connection_socket.close()
         return
 
-    data = b''
-    buffer = b'stub'
-    while len(buffer):
-        buffer = s.recv(MAX_REQUEST_LEN)
-        data += buffer
-    connection_socket.sendall(data)
+    if request_type == "CONNECT":
+        connection_socket.sendall(CONNECTION_ESTABLISHED_MESSAGE)
+        handle_request_response_exchange(connection_socket, s)
+    elif request_type == "GET" or request_type == "POST":
+        s.sendall(request)
+        handle_request_response_exchange(connection_socket, s)
+    else:
+        # Not Implemented
+        connection_socket.sendall(INTERNAL_SERVER_ERROR)
+        s.close()
+        connection_socket.close()
+        return
+
+
+def handle_request_response_exchange(client_socket, server_socket):
+    cur_sockets = [server_socket, client_socket]
+    while True:
+        ready_read, ready_write, errors = select.select(cur_sockets,
+                                                        [],
+                                                        cur_sockets,
+                                                        CONNECTION_TIMEOUT)
+        if not len(ready_read):
+            server_socket.close()
+            client_socket.close()
+            return
+
+        for sock in ready_read:
+            data = read_all(sock)
+
+            if not len(data):
+                cur_sockets.remove(sock)
+                sock.close()
+                continue
+            if sock is server_socket:
+                if VERBOSE:
+                    print(f"<= [{len(data)}]")
+                client_socket.sendall(data)
+            else:
+                if VERBOSE:
+                    print(f"=> [{len(data)}]")
+                server_socket.sendall(data)
+
+
+def is_url_filtered(url):
+    with open(filters, 'r') as f:
+        for line in filter(lambda x: len(x), f.readlines()):
+            if re.findall(rf'{line.rstrip()}', url):
+                return True
+    return False
+
+
+def read_all(sock):
+    # data = b''
+    # buffer = b'stub'
+    # while len(buffer):
+    #     try:
+    #         buffer = sock.recv(MAX_BUFFER_SIZE)
+    #     except socket.error:
+    #         break
+    #     data += buffer
+    # return data
+    # The code above doesn't work and I don't know why actually
+    try:
+        return sock.recv(MAX_BUFFER_SIZE)
+    except:
+        # print("ERROR WHILE READING SOCKET")
+        return b''
 
 
 def extract_config_from_request(request):
-    first_line = request.split('\n')[0]
+    splitted_request = request.decode('utf8').split('\n')
+    first_line = splitted_request[0]
     splitted_line = first_line.split(' ')
     request_type = splitted_line[0]
 
@@ -76,7 +143,7 @@ def extract_config_from_request(request):
         port = int(
             (rest_of_url[(port_pos + 1):])[:webserver_pos - port_pos - 1])
         webserver = rest_of_url[:port_pos]
-    return url, webserver, port
+    return url, webserver, port, request_type
 
 
 class Server:
@@ -91,13 +158,12 @@ class Server:
 
     def listen_for_client(self):
         while True:
-            (clientSocket,
+            (client_socket,
              client_address) = self.serverSocket.accept()
 
-            self.clients.append(clientSocket)
-
+            self.clients.append(client_socket)
             threading.Thread(target=handle_browser_request,
-                             args=(clientSocket,), daemon=True).start()
+                             args=(client_socket,), daemon=True).start()
 
     def shutdown(self):
         for sock in self.clients:
@@ -148,3 +214,5 @@ def main(args):
 
 if __name__ == "__main__":
     main(sys.argv)
+
+
